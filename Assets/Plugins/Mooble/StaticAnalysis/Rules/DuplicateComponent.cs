3b93a0e3690fc8d5b1c3b7136316ac728765e002
♗ nathan <elmoeleven@gmail.com>
2016-11-16 15:49:24 -0500

    Add `DuplicateComponent`.

---
+++ Assets/Plugins/Mooble/StaticAnalysis/Rules/DuplicateComponent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+using Mooble.StaticAnalysis.Violation;
+using UnityEngine;
+
+namespace Mooble.StaticAnalysis.Rules {
+  public class DuplicateComponent : Rule<GameObject> {
+    public const string NAME = "DuplicateComponent";
+
+    public DuplicateComponent() : base(NAME, ViolationLevel.Warning) {
+    }
+
+    public override List<IViolation> Handle(object thing) {
+      return this.Handle(thing as GameObject);
+    }
+
+    public override List<IViolation> Handle(GameObject thing) {
+      var violations = new List<IViolation>();
+      Component[] allComponents = thing.GetComponents<Component>();
+
+      var s = new Dictionary<string, int>();
+
+      foreach (var component in allComponents) {
+        var componentType = component.GetType().ToString();
+
+        if (s.ContainsKey(componentType)) {
+          s[componentType]++;
+        } else {
+          s[componentType] = 1;
+        }
+      }
+
+      foreach (var kvp in s) {
+        if (kvp.Value > 1) {
+          violations.Add(new DuplicateComponentViolation(this, thing.name, kvp.Key));
+        }
+      }
+
+      return violations;
+    }
+  }
+}
---
+++ Assets/Plugins/Mooble/StaticAnalysis/Violations/DuplicateComponentViolation.cs
@@ -0,0 +1,17 @@
+﻿namespace Mooble.StaticAnalysis.Violation {
+  public class DuplicateComponentViolation : IViolation {
+    private Rules.DuplicateComponent rule;
+    private string gameObjectName;
+    private string componentName;
+
+    public DuplicateComponentViolation(Rules.DuplicateComponent r, string o, string c) {
+      this.rule = r;
+      this.gameObjectName = o;
+      this.componentName = c;
+    }
+
+    public string Format() {
+      return this.rule.Level + " " + this.rule.Name + ": more than one " + this.componentName + " on " + this.gameObjectName;
+    }
+  }
+}
