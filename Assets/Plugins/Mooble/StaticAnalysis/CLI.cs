a84319201d011d131dd7301aca2e57b765d39748
Shahrin Khan <shahrin@uken.com>
2016-11-18 12:30:06 -0500

    Display full path for CLI formatting.

--- Assets/Plugins/Mooble/StaticAnalysis/CLI.cs
+++ Assets/Plugins/Mooble/StaticAnalysis/CLI.cs
@@ -1,73 +1,106 @@
 using System.Collections.Generic;
+using System.Text;
 
 using UnityEditor.SceneManagement;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Mooble.StaticAnalysis {
   public static class CLI {
-      public static void RunSceneAnalysis() {
-        var commandLineArgs = System.Environment.GetCommandLineArgs();
+    public static void RunSceneAnalysis() {
+      var commandLineArgs = System.Environment.GetCommandLineArgs();
 
-        var scenes = new List<Scene>();
+      var scenes = new List<Scene>();
 
-        foreach (var arg in commandLineArgs) {
-          Scene scene;
+      foreach (var arg in commandLineArgs) {
+        Scene scene;
 
-          try {
-            scene = EditorSceneManager.OpenScene(arg);
-          } catch (System.ArgumentException) {
-            continue;
-          }
-
-          if (scene.IsValid()) {
-            Log.Debug("Found valid scene: " + scene.name);
-            scenes.Add(scene);
-          }
+        try {
+          scene = EditorSceneManager.OpenScene(arg);
+        } catch (System.ArgumentException) {
+          continue;
         }
 
-        if (scenes.Count == 0) {
-          throw new System.Exception("No scenes provided; skipping static analysis.");
+        if (scene.IsValid()) {
+          Log.Debug("Found valid scene: " + scene.name);
+          scenes.Add(scene);
         }
+      }
 
-        var config = Mooble.Config.Config.LoadFromFile();
-        var sa = new StaticAnalysisBuilder(config).Get();
+      if (scenes.Count == 0) {
+        throw new System.Exception("No scenes provided; skipping static analysis.");
+      }
 
-        var stringBuilder = new System.Text.StringBuilder();
-        bool foundError = false;
+      var config = Mooble.Config.Config.LoadFromFile();
+      var sa = new StaticAnalysisBuilder(config).Get();
 
-        foreach (var scene in scenes) {
-          stringBuilder.Append("\nAnalyzing scene: " + scene.name);
+      var stringBuilder = new StringBuilder();
+      bool foundError = false;
 
-          foreach (var root in scene.GetRootGameObjects()) {
-            Dictionary<Rule, List<IViolation>> violations = sa.Analyze(root);
-            stringBuilder.Append("\n\tAnalyzing root object in scene: " + root.name);
+      foreach (var scene in scenes) {
+        stringBuilder.Append("\nAnalyzing scene: " + scene.name);
 
-            foreach (var kvp in violations) {
-              if (kvp.Value.Count == 0) {
-                continue;
+        foreach (var root in scene.GetRootGameObjects()) {
+          Dictionary<Rule, List<IViolation>> violations = sa.Analyze(root);
+          stringBuilder.Append("\n\tAnalyzing root object in scene: " + root.name);
+
+          foreach (var kvp in violations) {
+            if (kvp.Value.Count == 0) {
+              continue;
+            }
+
+            stringBuilder.Append("\n\t\tViolations for rule: ");
+            stringBuilder.Append(kvp.Key.Name);
+
+            foreach (var violation in kvp.Value) {
+              if (violation.Level == ViolationLevel.Error) {
+                foundError = true;
               }
 
-              stringBuilder.Append("\n\t\tViolations for rule: ");
-              stringBuilder.Append(kvp.Key.Name);
-
-              foreach (var violation in kvp.Value) {
-                if (violation.Level == ViolationLevel.Error) {
-                  foundError = true;
-                }
-
-                stringBuilder.Append("\n");
-                stringBuilder.Append("\t\t\t");
-                stringBuilder.Append(violation.Format());
-              }
+              stringBuilder.Append("\n");
+              stringBuilder.Append("\t\t\t");
+              stringBuilder.Append(FullPath(violation.GetObject()));
+              stringBuilder.Append(": ");
+              stringBuilder.Append(violation.Format());
             }
           }
         }
-
-        Log.Debug(stringBuilder.ToString());
-
-        if (foundError) {
-          throw new System.Exception("Error violation found!");
-        }
       }
+
+      Log.Debug(stringBuilder.ToString());
+
+      if (foundError) {
+        throw new System.Exception("Error violation found!");
+      }
+    }
+
+    public static string FullPath(UnityEngine.Object o) {
+      if (o is Component) {
+        return FullPath(o as Component);
+      } else {
+        return FullPath(o as GameObject);
+      }
+    }
+
+    public static string FullPath(Component c) {
+      return FullPath(c.gameObject);
+    }
+
+    public static string FullPath(GameObject gameObject) {
+      var path = new StringBuilder();
+
+      var transform = gameObject.transform;
+      path.Append(transform.name);
+
+      while (transform.parent != null) {
+        transform = transform.parent;
+        path.Insert(0, "/");
+        path.Insert(0, transform.name);
+      }
+
+      path.Insert(0, "/");
+
+      return path.ToString();
+    }
   }
 }
