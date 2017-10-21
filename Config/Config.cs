cd696ef9be804fafe610197ff6d1eaa280acb074
Shahrin Khan <gooble.sk@gmail.com>
2017-10-21 14:42:01 -0400

    Change default config directory for moobleconfig.json

--- Config/Config.cs
+++ Config/Config.cs
@@ -1,19 +1,41 @@
 #if UNITY_EDITOR
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-namespace Mooble.Config {
-  public class Config {
+namespace Mooble.Config
+{
+  public class Config
+  {
+    private static readonly string[] POTENTIAL_CONFIG_FILES = new[] {
+      "Assets/Plugins/Mooble/moobleconfig.json",
+      "moobleconfig.json"
+    };
+
     public string[] PrefabLocations;
     public StaticAnalysisRuleConfig[] Rules;
 
-    public static Config LoadFromFile(string fileName = "moobleconfig.json") {
-      string text = System.IO.File.ReadAllText(fileName);
-      return JsonUtility.FromJson<Config>(text);
+    public static Config LoadFromFile(string fileName = null)
+    {
+      List<string> fileNamesToTry = new List<string>();
+      fileNamesToTry.Add(fileName);
+      fileNamesToTry.AddRange(POTENTIAL_CONFIG_FILES);
+
+      foreach (string path in fileNamesToTry)
+      {
+        if (System.IO.File.Exists(path))
+        {
+          string text = System.IO.File.ReadAllText(path);
+          return JsonUtility.FromJson<Config>(text);
+        }
+      }
+
+      throw new ArgumentException("Could not not find Mooble config at " + fileName + " or any of the default locations: " + string.Join(", ", POTENTIAL_CONFIG_FILES));
     }
 
     [Serializable]
-    public class StaticAnalysisRuleConfig {
+    public class StaticAnalysisRuleConfig
+    {
       public string Assembly;
       public string Name;
       public string ViolationLevel;
